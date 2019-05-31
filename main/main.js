60f77092eb69e8d475e86ebb5b035d3d34c4ca40
luominyu <43443514+luominyu@users.noreply.github.com>
2019-05-31 23:43:10 +0800

    taxi price

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
-    // write your code here...
+     let inputs = {distance，parkTime};
+     var distance = new distance;
+     var parkTime = new parkTime;
+     var price=0;
+     if（distance<=2）{
+     price = 6+0.25*parkTime;//路程两公里以内
+     }else if（distance>2&&distance<=8）{
+         price = 6+(distance-2)*0.8+0.25*parkTime;
+     }//路程超出两公里但小于八公里
+    else if（distance>8）{
+         price = 6+6*0.8+(distance-8)*0.8*0.5+0.25*parkTime;
+     }//路程大于八公里
+    document.write(Math.round(price) + "<br />")；
     return "price";
 };
